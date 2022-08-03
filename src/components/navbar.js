5e3a9c4d577bbd95d5e92d0bc941d47706db2f02
Precious Adedibu <adedibuprecious@gmail.com>
2022-08-04 00:36:44 +0100

    update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,40 +1,41 @@
-import { Link, useNavigate } from 'react-router-dom'
-import './navbar.css'
+import { Link, useNavigate } from "react-router-dom";
+import "./navbar.css";
 
 function NavBar() {
-	const navigate = useNavigate()
-	const loggedIn = localStorage.getItem('loggedIn')
+  const navigate = useNavigate();
+  const loggedIn = localStorage.getItem("loggedIn");
+  console.log(loggedIn);
 
-	const handleclick = (e) => {
-		e.preventDefault()
-		console.log('I will redirect to AWS Cognito')
-		localStorage.setItem('loggedIn', true)
-		navigate('/userdashboard/user-dashboard.js')
-	}
+  const handleclick = (e) => {
+    e.preventDefault();
+    console.log("I will redirect to AWS Cognito");
+    localStorage.setItem("loggedIn", true);
+    navigate("/userdashboard/user-dashboard.js?id=");
+  };
 
-	function handleSignout(e) {
-		e.preventDefault()
-		console.log('signed out')
-		localStorage.removeItem('loggedIn')
-	}
+  function handleSignout(e) {
+    e.preventDefault();
+    console.log("signed out");
+    localStorage.removeItem("loggedIn");
+  }
 
-	return (
-		<div id='topnav'>
-			{loggedIn ? (
-				<button
-					onClick={(e) => handleclick(e)}
-					to='/signin/signin.js'
-					className='pages'
-				>
-					Sign In
-				</button>
-			) : (
-				<button onClick={(e) => handleSignout(e)} className='pages'>
-					Sign Out
-				</button>
-			)}
-		</div>
-	)
+  return (
+    <div id="topnav">
+      {loggedIn ? (
+        <button onClick={(e) => handleSignout(e)} className="pages">
+          Sign Out
+        </button>
+      ) : (
+        <button
+          onClick={(e) => handleclick(e)}
+          to="/signin/signin.js"
+          className="pages"
+        >
+          Sign In
+        </button>
+      )}
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
--- src/components/userdashboardbus/userdashboard-bus.js
+++ src/components/userdashboardbus/userdashboard-bus.js
@@ -1,86 +1,86 @@
-import { useState, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import './userdashboard-bus.css'
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./userdashboard-bus.css";
 
 function BusDashboard() {
-	const navigate = useNavigate()
-	const [busData, setBusData] = useState({})
+  const navigate = useNavigate();
+  const [busData, setBusData] = useState({});
 
-	useEffect(() => {
-		const busData = JSON.parse(localStorage?.getItem('busData'))
-		if (busData) {
-			setBusData(busData)
-		}
-	}, [])
+  useEffect(() => {
+    const busData = JSON.parse(localStorage?.getItem("busData"));
+    if (busData) {
+      setBusData(busData);
+    }
+  }, []);
 
-	function handleSubmit(event) {
-		event.preventDefault()
-		localStorage.setItem('busData', JSON.stringify(busData))
-	}
+  function handleSubmit(event) {
+    event.preventDefault();
+    localStorage.setItem("busData", JSON.stringify(busData));
+  }
 
-	function handleBack() {
-		navigate('/userdashboardairplane/user-dasboard-airplane.js')
-	}
+  function handleBack() {
+    navigate("/userdashboardairplane/user-dasboard-airplane.js");
+  }
 
-	function handleChange(event) {
-		const name = event.target.name
-		const value = event.target.value
-		setBusData((values) => ({ ...values, [name]: value }))
-	}
+  function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    setBusData((values) => ({ ...values, [name]: value }));
+  }
 
-	return (
-		<div className='Usrb'>
-			<div id='userbusdash'>
-				<div id='icons'>
-					<i className='fa fa-home iconcolor fsizeh'></i>
-					<i className='fa fa-car iconcolor fsize'></i>
-					<i className='fa fa-bus active iconcolor fsize'></i>
-					<i className='fa fa-plane iconcolor fsize'></i>
-					<i className='fa fa-train iconcolor fsize'></i>
-				</div>
+  return (
+    <div className="Usrb">
+      <div id="userbusdash">
+        <div id="icons">
+          <i className="fa fa-home iconcolor fsizeh"></i>
+          <i className="fa fa-car iconcolor fsize"></i>
+          <i className="fa fa-bus active iconcolor fsize"></i>
+          <i className="fa fa-plane iconcolor fsize"></i>
+          <i className="fa fa-train iconcolor fsize"></i>
+        </div>
 
-				<div id='datareg'>
-					<form action='' onSubmit={handleSubmit} id='powerdata'>
-						<div id='elect'>
-							<label htmlFor='elect' className=''>
-								Distance Travelled
-							</label>
-							&nbsp;&nbsp;
-							<input
-								type='text'
-								name='elect'
-								value={busData.elect || ''}
-								onChange={handleChange}
-								className='form-elect-input'
-							/>
-							&nbsp;&nbsp;
-							<label htmlFor='kwh' className=''>
-								Km
-							</label>
-							<br></br>
-						</div>
+        <div id="datareg">
+          <form action="" onSubmit={handleSubmit} id="powerdata">
+            <div id="elect">
+              <label htmlFor="elect" className="">
+                Distance Travelled
+              </label>
+              &nbsp;&nbsp;
+              <input
+                type="text"
+                name="elect"
+                value={busData.elect || ""}
+                onChange={handleChange}
+                className="form-elect-input"
+              />
+              &nbsp;&nbsp;
+              <label htmlFor="kwh" className="">
+                Km
+              </label>
+              <br></br>
+            </div>
 
-						<div id='btn'>
-							<button type='reset' id='reset'></button>
-							<button type='submit' id='calc' onClick={() => handleBack()}>
-								{' '}
-								Back
-							</button>
-							<button type='submit' id='calc' onClick={(e) => handleSubmit(e)}>
-								NEXT
-							</button>
-						</div>
-					</form>
-				</div>
+            <div id="btn">
+              <button type="reset" id="reset"></button>
+              <button type="submit" id="calc" onClick={() => handleBack()}>
+                {" "}
+                Back
+              </button>
+              <button type="submit" id="calc" onClick={(e) => handleSubmit(e)}>
+                <Link to="/userdashboardcar/user-dasboard-car.js">Next</Link>
+              </button>
+            </div>
+          </form>
+        </div>
 
-				<div id='winclose'>
-					<button type='submit' name='close' id='btnclose'>
-						<i className='fa fa-times-circle-o'></i>
-					</button>
-				</div>
-			</div>
-		</div>
-	)
+        <div id="winclose">
+          <button type="submit" name="close" id="btnclose">
+            <i className="fa fa-times-circle-o"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default BusDashboard
+export default BusDashboard;
